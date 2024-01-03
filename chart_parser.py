69d057430b963dabb4b95d38cecb818f03152b98
James Nichols <jnichols.tigley@gmail.com>
2024-01-03 18:32:06 +0000

    Rubbish documentation but somehow got it working.

--- chart_parser.py
+++ chart_parser.py
@@ -1,13 +1,21 @@
 import re
 import ast
 from dataclasses import dataclass
+import pygame
 SECTION_PATTERN = re.compile("\[\w+\]")
 
 @dataclass
 class Note:
     pos: int
-    key: int
+    track: int
     duration : int
+    seconds : float
+
+@dataclass
+class Tempo:
+    bpm : float
+    secondsIn : float
+    position : int
 
 class Song:
     def __init__(self, filepath : str):
@@ -19,11 +27,10 @@ class Song:
         self.timesig = "4 4"
         self.tempoData = {}
         self.noteData = {}
-        self.notesPositionList = []
-        self.lastPassedNotePositionIndex = None
-        self.tempo = 120000
+        self.notesTimeList = []
+        self.lastPassedNoteTimeIndex = None
         self.loadSections()
-        self.updateTempo(0)
+        self.music = None
     
     def __parseData(self, data):
         try:
@@ -42,58 +49,70 @@ class Song:
         self.songName = songData["Name"]
         self.artistName = songData["Artist"]
         self.resolution = songData["Resolution"]
+
+        self.music = pygame.mixer.music.load(self.musicPath)
     
     def __loadSyncTrack(self, lines):
         self.tempoData = {}
+        firstTempo = True
         for line in lines:
             if "=" not in line:
                 continue
             position, data = line.split("=")
             dataSplit = data.strip().split()
             if dataSplit[0] == "B":
-                self.tempoData[int(position)] = int(dataSplit[1])
+                if firstTempo:
+                    firstTempo = False
+                    self.tempoData[int(position)] = Tempo(int(dataSplit[1])/1000,0,0)
+                else:
+                    secondsIn = self.getSecondsIn(int(position))
+                    self.tempoData[int(position)] = Tempo(int(dataSplit[1])/1000,secondsIn,int(position))
 
     def __loadNoteTrack(self, lines):
         self.noteData = {}
-        self.notesPositionList = []
+        self.notesTimeList = []
         for line in lines:
             if "=" not in line:
                 continue
             position, data = line.split("=")
+            position = int(position)
             dataSplit = data.strip().split()
             if dataSplit[0] in ["E","S"]:
                 continue
-            _, key, duration = dataSplit
-            self.noteData[int(position)] = Note(int(position), key, duration)
-        
-        self.notesPositionList = list(self.noteData.keys())
+            _, track, duration = dataSplit
+            self.noteData[self.getSecondsIn(position)] = Note(position, int(track), int(duration), self.getSecondsIn(position))
 
-    def updateTempo(self, position):
-        for pos, tempo in self.tempoData.items():
-            if position >= pos:
-                self.tempo = tempo
+        self.notesTimeList = list(self.noteData.keys())
+
+    def getSecondsIn(self, position : int):
+        tempo = self.getTempo(position)
+        tempoPosition, bpm, tempoSeconds = tempo.position, tempo.bpm, tempo.secondsIn
+        deltaTicks = position - tempoPosition
+        if position == tempoPosition:
+            return tempoSeconds
+        secondsIn = ((60 / bpm) * (deltaTicks / self.resolution)) + tempoSeconds
+        return secondsIn
     
-    def getPosition(self, elapsedTime : int):
-        elapsedMinutes = elapsedTime / 1000 / 60
-        return elapsedMinutes * (self.tempo / 1000) * self.resolution
+    def getTempo(self, position : int):
+        currentTempo = Tempo(120000,0,0)
+        for tempoPosition, tempo in self.tempoData.items():
+            if position >= tempoPosition:
+                currentTempo = tempo
+        return currentTempo
     
-    def getNotes(self, minimumPos, maximumPos):
-        checkedIndex = self.lastPassedNotePositionIndex if self.lastPassedNotePositionIndex else 0
+    def getNotes(self, minimumTime, maximumTime):
+        checkedIndex = self.lastPassedNoteTimeIndex if self.lastPassedNoteTimeIndex else 0
         notes = []
-        while checkedIndex < len(self.notesPositionList):
-            position = self.notesPositionList[checkedIndex]
-
-            if position >= maximumPos:
+        while checkedIndex < len(self.notesTimeList):
+            time = self.notesTimeList[checkedIndex]
+            if time >= maximumTime:
                 return notes
-
-            if position >= minimumPos:
-                notes.append(self.noteData[position])
-            
-            if position < minimumPos:
-                self.lastPassedNotePositionIndex = checkedIndex
-            
+            if time >= minimumTime:
+                notes.append(self.noteData[time])
+            if time < minimumTime:
+                self.lastPassedNoteTimeIndex = checkedIndex
             checkedIndex += 1
-        
+
         return notes
 
     def loadSections(self):
