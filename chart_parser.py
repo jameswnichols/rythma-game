856d79d849a37c1a7f7822e67eb9ee08235542b4
James Nichols <jnichols.tigley@gmail.com>
2024-01-03 13:54:03 +0000

    Work on file parsing.

--- chart_parser.py
+++ chart_parser.py
@@ -7,18 +7,21 @@ class File:
         self.filepath = filepath
         self.Resolution = None
         self.musicPath = None
+        self.loadSections()
     
     def loadSections(self):
         fileData = None
+        currentSection = None
         sections = {}
         with open(self.filepath, "r") as f:
             fileData = f.readlines()
-        
         for line in fileData:
-            
-            result = SECTION_PATTERN.match(line)
+            line = line.strip()
+            result = SECTION_PATTERN.findall(line)
             if not result:
+                sections[currentSection].append(line)
                 continue
-
-            print(result)
+            currentSection = str(result).replace("[","").replace("]","")
+            sections[currentSection] = []
+            print(currentSection)
         
