f9168100932c15b5e0527c3324ebac14f3ba0622
James Nichols <jnichols.tigley@gmail.com>
2024-01-03 15:59:44 +0000

    Start work on note rendering.

--- rythma.py
+++ rythma.py
@@ -14,22 +14,17 @@ pixelScreenSize = screenSize/config.PIXEL_REDUCTION_FACTOR
 KEYS = {pygame.K_a:[0, 1],pygame.K_s:[1, 2],pygame.K_d:[2, 3],pygame.K_SEMICOLON:[3, 4],pygame.K_QUOTE:[4, 5]}
 
 if __name__ == "__main__":
-
     screen = pygame.display.set_mode(screenSize,pygame.RESIZABLE)
-
     clock = pygame.time.Clock()
-
     pixelScreen = pygame.Surface(pixelScreenSize)
-
-    songFile = chart_parser.File(config.CHART_PATH)
-
+    song = chart_parser.Song(config.CHART_PATH)
     board = perspective.Board(pixelScreen, tracks=5)
 
     running = True
-
     dt = 0
-
     holdValue = 0
+    elapsedTime = 0
+    position = 0
 
     while running:
         screen.fill(config.BACKGROUND_COLOUR)
@@ -56,6 +51,18 @@ if __name__ == "__main__":
         board.update(pixelScreenSize)
         board.updateBarrierPoints([i for i in range(board.tracks+1)])
         board.render(pixelScreen)
+
+        #Notes
+        headerPosition = song.getPosition(elapsedTime*1000)
+        song.updateTempo(headerPosition)
+        endPosition = song.getPosition((elapsedTime*1000) + config.SONG_LOOKAHEAD_MS)
+
+        foundNotes = song.getNotes(headerPosition, endPosition)
+
+        for note in foundNotes:
+            closeToHeader = 1 - (note.pos / endPosition) #1 is at header, 0 is far away
+            print(relativeToHeader)
+
         
         #Upscale Pixel Screen
         pygame.transform.scale(pixelScreen,screenSize, screen)
@@ -64,8 +71,9 @@ if __name__ == "__main__":
 
         dt = clock.tick_busy_loop(60)/1000
 
+        elapsedTime += dt
         holdValue = holdValue + dt if holdValue + dt < 2 * math.pi else 0
 
-        pygame.display.set_caption(f"FPS - {round(clock.get_fps(), 1)}")
+        pygame.display.set_caption(f"FPS - {round(clock.get_fps(), 1)} POS - {headerPosition} SEC - {int(elapsedTime)}")
 
     pygame.quit()
