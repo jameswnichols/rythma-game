07875da453a643bb3c0cf81f932b74a5ac6c6e9b
James Nichols <jnichols.tigley@gmail.com>
2024-01-13 20:29:40 +0000

    Work on user input, currently a bit shaky.

--- scoring.py
+++ scoring.py
@@ -25,17 +25,46 @@ class Scoring:
         for track, key in config.TRACK_BINDS.items():
             keyPressedThisFrame = heldKeys[key]
             if self.tracks[track].justPressed and keyPressedThisFrame:
-                print(f"HOLDING TRACK {track}")
                 self.tracks[track].justPressed = False
                 self.tracks[track].isHeld = True
             elif ((not self.tracks[track].justPressed) and (not self.tracks[track].isHeld)) and keyPressedThisFrame:
-                print(f"PRESSED TRACK {track}")
                 self.tracks[track].justPressed = True
                 self.tracks[track].isHeld = False
             elif (not keyPressedThisFrame) and (self.tracks[track].justPressed or self.tracks[track].isHeld):
-                print(f"LIFTED TRACK {track}")
                 self.tracks[track].justPressed = False
                 self.tracks[track].isHeld = False
 
     def checkNotes(self, foundNotes : list[chart_parser.Note], scoreBarSeconds : float, dt : float):
-        pass
+        pressedInLane = {x : False for x in self.tracks}
+        for note in foundNotes:
+            graceSeconds = 0 if note.beenPressed else config.NOTE_GRACE_MS/1000
+            noteHeadStart = note.startSeconds - graceSeconds - (config.NOTE_DEBOUNCE_TIME_MS/1000)
+            noteHeadEnd = note.startSeconds + (2*(config.NOTE_DEPTH_MS/1000)) + graceSeconds + (config.NOTE_DEBOUNCE_TIME_MS/1000)
+            noteTailEnd = note.endSeconds
+            noteWithinRange = noteHeadStart <= scoreBarSeconds <= noteHeadEnd
+            if not note.beenPressed and noteWithinRange and self.tracks[note.track].justPressed: #
+                note.beenPressed = True
+                self.hitNotes += 1
+                self.currentStreak += 1
+                pressedInLane[note.track] = True
+                print(f"Note Hit : {note.pos}")
+            elif note.beenPressed and noteWithinRange and self.tracks[note.track].justPressed:
+                self.currentStreak = 0
+                print(f"Pressed Same Note Twice : {note.pos}")
+            elif not note.beenPressed and noteHeadEnd < scoreBarSeconds:
+                note.beenPressed = True
+                self.currentStreak = 0
+                print("Missed Note")
+        
+        hasStruck = False
+        for track, notePressed in pressedInLane.items():
+            if not notePressed and self.tracks[track].justPressed:
+                hasStruck = True
+                break
+        
+        if hasStruck:
+            self.currentStreak
+            print("Strike")
+        
+                
+            
